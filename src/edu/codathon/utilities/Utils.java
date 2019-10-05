f522eaa377fae85154770ae6c46199245afa24b5
Quentin-D-NM <qdye@cnm.edu>
2019-10-04 22:15:42 -0600

    Added parseComment method.

--- src/edu/codathon/utilities/Utils.java
+++ src/edu/codathon/utilities/Utils.java
@@ -1,10 +1,43 @@
 package edu.codathon.utilities;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Utils {
+
+  public static List<List<String>> parseComment(String fileName) {
+    List<List<String>> parsed = new ArrayList<>();
+    BufferedReader reader;
+    final String USER = "@therealDonaldTrump";
+    final int TEXT = 1; // Known value for the tweet
+    final int TIME = 2; // Known value for time stamp
+
+    try {
+      reader = new BufferedReader(new FileReader(fileName));
+      String line = reader.readLine();
+      while(line != null) {
+        List<String> temp = new ArrayList<>();
+        String[] split = line.split(",");
+        temp.add(split[TEXT]);
+        temp.add(split[TIME]);
+        temp.add(USER);
+        parsed.add(temp);
+        line = reader.readLine();
+      }
+      reader.close();
+    }catch (IOException e) {
+      //Ignored
+    }
+    return parsed;
+  }
+
+
 
   private static MessageDigest digest;
 
